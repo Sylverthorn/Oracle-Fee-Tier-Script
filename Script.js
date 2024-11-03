c2df29c49a434f996e970ded298261413982d598
Korichi Yanis <97361294+Sylverthorn@users.noreply.github.com>
2024-11-03 17:03:35 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,13 +1,10 @@
-// Fonction pour gérer le clic sur "Créer" et "Continuer à travailler"
 function automateButtons() {
-    // Vérifie si le bouton "Continuer à travailler" est présent
     var continueButton = Array.from(document.querySelectorAll('button')).find(button => button.textContent.trim() === 'Continuer à travailler');
     
     if (continueButton) {
         continueButton.click();
         console.log("Bouton 'Continuer à travailler' cliqué");
     } else {
-        // Si le bouton "Continuer à travailler" n'est pas présent, clique sur "Créer"
         var createButton = Array.from(document.querySelectorAll('button')).find(button => button.textContent.trim() === 'Créer');
         if (createButton) {
             createButton.click();
@@ -16,5 +13,5 @@ function automateButtons() {
     }
 }
 
-// Exécuter la fonction toutes les 30 secondes (30 000 millisecondes)
+
 setInterval(automateButtons, 30000);
